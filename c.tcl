1a84da1e1904fd8c586a1268923066e94ecc77bf
tobyshooters <cristobal.sciutto@gmail.com>
2023-01-14 14:15:44 -0500

    init

---
+++ c.tcl
@@ -0,0 +1,190 @@
+namespace eval c {
+    variable nextHandle 0
+    proc create {} {
+
+        variable nextHandle
+        set handle "c[incr nextHandle]"
+
+        uplevel [list namespace eval $handle {
+            variable prelude {
+                #include <tcl.h>
+                #include <inttypes.h>
+                #include <stdint.h>
+            }
+            variable code [list]
+            variable procs [dict create]
+
+            variable argtypes {
+                int { expr {{ Tcl_GetIntFromObj(interp, $obj, &$argname); }}}
+                char* { expr {{ $argname = Tcl_GetString($obj); }} }
+                Tcl_Obj* { expr {{ $argname = $obj; }}}
+                default {
+                    if {[string index $argtype end] == "*"} {
+                        expr {{
+                            if (sscanf(Tcl_GetString($obj), "($argtype) 0x%p", &$argname) != 1) {
+                                return TCL_ERROR;
+                            }
+                        }}
+                    } else {
+                        error "Unrecognized argtype $argtype"
+                    }
+                }
+            }
+            ::proc argtype {t h} {
+                variable argtypes
+                set argtypes [linsert $argtypes 0 $t [subst {expr {{$h}}}]]
+            }
+
+            variable rtypes {
+                int { expr {{
+                    Tcl_SetObjResult(interp, Tcl_NewIntObj(rv));
+                    return TCL_OK;
+                }}}
+                char* { expr {{ Tcl_SetObjResult(interp, Tcl_ObjPrintf("%s", rv)); return TCL_OK; }} }
+                Tcl_Obj* { expr {{
+                    Tcl_SetObjResult(interp, rv);
+                    return TCL_OK;
+                }}}
+                default {
+                    if {[string index $rtype end] == "*"} {
+                        expr {{
+                            Tcl_SetObjResult(interp, Tcl_ObjPrintf("($rtype) 0x%" PRIxPTR, (uintptr_t) rv));
+                            return TCL_OK;
+                        }}
+                    } else {
+                        error "Unrecognized rtype $rtype"
+                    }
+                }
+            }
+            ::proc rtype {t h} {
+                variable rtypes
+                set rtypes [linsert $rtypes 0 $t [subst {expr {{$h}}}]]
+            }
+
+            ::proc include {h} {
+                variable code
+                lappend code "#include $h"
+            }
+            ::proc code {newcode} { variable code; lappend code $newcode; list }
+            ::proc struct {type fields} {
+                variable code
+                lappend code [subst {
+                    typedef struct $type $type;
+                    struct $type {
+                        $fields
+                    };
+                }]
+            }
+
+            ::proc "proc" {name args rtype body} {
+                # puts "$name $args $rtype $body"
+                variable argtypes
+                variable rtypes
+
+                set arglist [list]
+                set argnames [list]
+                set loadargs [list]
+                for {set i 0} {$i < [llength $args]} {incr i 2} {
+                    set argtype [lindex $args $i]
+                    set argname [lindex $args [expr {$i+1}]]
+                    lappend arglist "$argtype $argname"
+                    lappend argnames $argname
+
+                    if {$argtype == "Tcl_Interp*" && $argname == "interp"} { continue }
+
+                    set obj [subst {objv\[1 + [llength $loadargs]\]}]
+                    lappend loadargs [subst {
+                        $argtype $argname;
+                        [subst [switch $argtype $argtypes]]
+                    }]
+                }
+                if {$rtype == "void"} {
+                    set saverv [subst {
+                        $name ([join $argnames ", "]);
+                        return TCL_OK;
+                    }]
+                } else {
+                    set saverv [subst {
+                        $rtype rv = $name ([join $argnames ", "]);
+                        [subst [switch $rtype $rtypes]]
+                    }]
+                }
+
+                set uniquename [string map {":" "_"} [uplevel [list namespace current]]]__$name
+                variable procs
+                dict set procs $name [subst {
+                    static $rtype $name ([join $arglist ", "]) {
+                        $body
+                    }
+
+                    static int [set name]_Cmd(ClientData cdata, Tcl_Interp* interp, int objc, Tcl_Obj* const objv\[\]) {
+                        if (objc != 1 + [llength $loadargs]) {
+                            Tcl_SetResult(interp, "Wrong number of arguments to $name", NULL);
+                            return TCL_ERROR;
+                        }
+                        [join $loadargs "\n"]
+                        $saverv
+                    }
+                }]
+            }
+
+            variable cflags [switch $tcl_platform(os) {
+                Darwin { expr { [file exists "$::tcl_library/../../Tcl"] ?
+                                [list -I$::tcl_library/../../Headers $::tcl_library/../../Tcl] :
+                                [list -I$::tcl_library/../../include $::tcl_library/../libtcl8.6.dylib]
+                            } }
+                Linux { list -I/usr/include/tcl8.6 -ltcl8.6 }
+            }]
+            ::proc cflags {args} { variable cflags; lappend cflags {*}$args }
+            ::proc compile {} {
+                variable prelude
+                variable code
+                variable procs
+                variable cflags
+
+                set init [subst {
+                    int Cfile_Init(Tcl_Interp* interp) {
+                        [join [lmap name [dict keys $procs] { subst {
+                            Tcl_CreateObjCommand(interp, "[uplevel [list namespace current]]::$name", [set name]_Cmd, NULL, NULL);
+                        }}] "\n"]
+                        return TCL_OK;
+                    }
+                }]
+                set sourcecode [join [list \
+                                          $prelude \
+                                          {*}$code \
+                                          {*}[dict values $procs] \
+                                          $init \
+                                         ] "\n"]
+
+                # puts "=====================\n$sourcecode\n====================="
+
+                set cfd [file tempfile cfile cfile.c]; puts $cfd $sourcecode; close $cfd
+                exec cc -Wall -g -shared -fPIC {*}$cflags $cfile -o [file rootname $cfile][info sharedlibextension]
+                load [file rootname $cfile][info sharedlibextension] cfile
+            }
+
+            namespace export *
+            namespace ensemble create
+        }]
+        return $handle
+    }
+
+    set loader [create]
+    $loader include <dlfcn.h>
+    $loader proc loadlibImpl {char* filename} void* {
+        // TODO: report dlerror error
+        // TODO: better shadowing handling
+        return dlopen(filename, RTLD_NOW | RTLD_GLOBAL);
+    }
+    $loader proc loadlibError {} char* { return dlerror(); }
+    $loader compile
+    proc loadlib {filename} {
+        if {[loadlibImpl $filename] == "(void*) 0x0"} {
+            error "Failed to dlopen $filename: [loadlibError]"
+        }
+    }
+
+    namespace export *
+    namespace ensemble create
+}
---
+++ ping.tcl
@@ -0,0 +1,94 @@
+source "c.tcl"
+
+set cc [c create]
+
+$cc include <stdio.h>
+$cc include <stdlib.h>
+$cc include <string.h>
+$cc include <arpa/inet.h>
+$cc include <sys/types.h>
+$cc include <sys/socket.h>
+$cc include <netdb.h>
+$cc include <unistd.h>
+
+namespace eval tickle {
+    $cc proc serve {} void {
+        struct addrinfo hints;
+        memset(&hints, 0, sizeof hints);
+        hints.ai_family = AF_INET;       // ipv4
+        hints.ai_socktype = SOCK_STREAM; // tcp
+        hints.ai_flags = AI_PASSIVE;
+
+        struct addrinfo *res;
+        getaddrinfo(NULL, "3490", &hints, &res);
+
+        int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+        bind(sockfd, res->ai_addr, res->ai_addrlen);
+        listen(sockfd, 10);
+
+        // Wait for incoming connections
+        struct sockaddr_storage client;
+        socklen_t size = sizeof client;
+        int fd = accept(sockfd, (struct sockaddr *) &client, &size);
+
+        // Send message to first client
+        char* msg = "hello world!";
+        send(fd, msg, strlen(msg), 0);
+
+        close(fd);
+        close(sockfd);
+        freeaddrinfo(res);
+    }
+
+    $cc proc join {char* hostname} int {
+        struct addrinfo hints;
+        memset(&hints, 0, sizeof hints);
+        hints.ai_family = AF_INET;       // ipv4
+        hints.ai_socktype = SOCK_STREAM; // tcp
+
+        struct addrinfo *res;
+        getaddrinfo(hostname, "3490", &hints, &res);
+
+        int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+        connect(sockfd, res->ai_addr, res->ai_addrlen);
+
+        freeaddrinfo(res);
+        return sockfd;
+    }
+
+    $cc proc ping {int sockfd} void {
+        // Get name of server
+        struct sockaddr_in name;
+        socklen_t size;
+        getpeername(sockfd, (struct sockaddr *)&name, &size);
+
+        char ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &(name.sin_addr), ip, sizeof ip);
+
+        // Get message
+        char reply[1024];
+        recv(sockfd, reply, sizeof reply, 0);
+        printf("%s: %s\n", ip, reply);
+    }
+
+    $cc compile
+}
+
+# package require Thread
+
+# set server [::thread::create -joinable {
+#     source ping.tcl
+#     tickle::serve 
+#     ::thread::exit
+# }]
+
+# set client [::thread::create -joinable {
+#     source ping.tcl
+#     set fd [tickle::join "localhost"]
+#     puts $fd
+#     tickle::ping $fd
+#     ::thread::exit
+# }]
+
+# ::thread::join $server
+# ::thread::join $client
